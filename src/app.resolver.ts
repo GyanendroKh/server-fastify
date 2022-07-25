5657b41a6e8cdec56d36fadcb0085b60c277b8e7
Gyanendro Kh <16163526+GyanendroKh@users.noreply.github.com>
2022-07-25 09:17:24 +0530

    Upload

--- src/app.resolver.ts
+++ src/app.resolver.ts
@@ -1,9 +1,30 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { createWriteStream } from 'fs';
+import { GraphQLUpload, FileUpload } from 'graphql-upload';
+import * as path from 'path';
+import * as stream from 'stream';
+import { promisify } from 'util';
+
+const pipeline = promisify(stream.pipeline);
 
 @Resolver()
 export class AppResolver {
   @Query(() => String)
   hello() {
     return 'Hello World!';
+  }
+
+  @Query(() => String)
+  async upload(
+    @Args({ name: 'image', type: () => GraphQLUpload })
+    image: FileUpload
+  ) {
+    const rs = image.createReadStream();
+
+    const ws = createWriteStream(path.join('uploads', image.filename));
+
+    await pipeline(rs, ws);
+
+    return image.filename;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter } from '@nestjs/platform-fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from '@nestjs/platform-fastify';
+import MercuriusUpload from 'mercurius-upload';
 import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 const bootstrap = async () => {
   const fastify = new FastifyAdapter();
-  const app = await NestFactory.create(AppModule, fastify, {
-    bufferLogs: true
-  });
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    fastify,
+    {
+      bufferLogs: true
+    }
+  );
   app.useLogger(app.get(Logger));
+  app.register(MercuriusUpload);
 
   await app.listen(3000, '0.0.0.0', () => {
     app.get<Logger>(Logger).log('Listening at port :3000', 'Bootstrap');
